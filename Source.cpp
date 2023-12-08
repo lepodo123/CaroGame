10587c7fd286cf442db469891b15d42b02ef4d9f
LePhamVoHieu <116814338+lepodo123@users.noreply.github.com>
2023-12-08 23:14:34 +0700

    Revert "add function input|handel|checkduplicate"

--- Source.cpp
+++ Source.cpp
@@ -7,19 +7,6 @@ struct Point {
 	int player=-1;
 };
 
-bool checkDuplicate(Point point, Point p[]) {
-	for (int i = 0; i < 25; i++) {
-
-		if (p[i].x == point.x && p[i].y == point.y) {
-			if (p[i].player != -1) {
-				return true;
-			}
-      		}
-	}
-	return false;
-}
-
-
 bool checkDiagonal(Point point, Point p[]) {
 	if (point.x == 3 && point.y == 3 && point.player != -1) {
 		for (int i = 0; i < 25; i++) {
@@ -103,20 +90,6 @@ bool checkDiagonal(Point point, Point p[]) {
 	}
 	return false;
 }
-
-Point Input(int player) {
-	Point p;
-	
-		cout << "Luot danh cua choi " << player;
-		cout << "\nNhap dong(1-5): ";
-		cin >> p.x;
-		cout << "\nNhap cot(1-5): ";
-		cin >> p.y;
-		p.player = player % 2;
-
-	return p;
-}
-
 bool checkWin(Point p[],Point point) {
 		
 	if (checkRow(point, p) || checkColumn(point, p) || checkDiagonal(point,p)) {
@@ -291,42 +264,7 @@ bool checkColumn(Point point, Point p[]) {
 	}
 	return false;
 }
-void Handle() {
-	Point p[25];
-	DrawBoard(p);
-	int player = 0;
-	for (int i = 0; i < 25; i++) {
-		player = i % 2;
-		while(true){
-			
-			Point point;
-			while (true)
-			{
-				point = Input(player);
-				if (point.x > 0 && point.x < 6 && point.y> 0 && point.y < 5) {
-					break;
-				}
-				DrawBoard(p);
-			}
-			if (checkDuplicate(point, p) == false) {
-				p[i] = point;
-				DrawBoard(p);
-				break;
-			}
-			
-			DrawBoard(p);
-			cout << "Da co nguoi choi danh vao vi tri do.\nMoi danh lai." << endl;
-		}
-		
-		if (checkWin(p, p[i])) {
-			cout << "NGUOI CHOI " << player << " THANG.";
-			break;
-		}
-	}
-	
-	
-	
-}
+
 int main() {
 	Handle();
 	cout << "\n";
