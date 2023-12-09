f4560d7b3336fbf603d9bea73f90a403a9b1609b
tuilakhoa2512 <fbkhoqkhoa2512@gmail.com>
2023-12-09 17:18:16 +0700

    add function resize board

--- Source.cpp
+++ Source.cpp
@@ -7,44 +7,51 @@ struct Point {
 	int player=-1;
 };
 
-void DrawBoard(Point p[]) {
+void DrawBoard(Point p[],int size) {
 	system("cls");
-			for (int i = 0; i < 5; i++) {
-			if (i == 0) {
-				cout << "\n";
-				for (int z = 0; z < 6; z++) {
-					if (z == 0) {
-						cout << "     |";
-					}
-					else {
-						cout << "  " << z << "  |";
-					}
-				}
-				cout << "\n------------------------------------";
-			}
+	for (int i = 0; i < size; i++) {
+		if (i == 0) {
 			cout << "\n";
-			for (int j = 0; j < 6; j++) {
-				if (j == 0) {
-					cout << "  " << i+1 << "  |";
+			for (int z = 0; z < size+1; z++) {
+				if (z == 0) {
+					cout << "      |";
 				}
 				else {
-					char c = ' ';
-					for (int l = 0; l < 25; l++) {
-						
-						if (p[l].x == i+1 && p[l].y==j) {
-							
-							if (p[l].player == 0) {
-								c = 'X';
-							}
-							else {
-								c = 'O';
-							}
-						}		
-					}
-					cout <<"  " << c << "  |";
+					cout << "  " << z << "   |";
 				}
 			}
-			cout << "\n------------------------------------";
+			cout << "\n";
+			for (int z = 0; z < size; z++) {
+				cout << "----------";
+			}
+			
+		}
+		cout << "\n";
+		for (int j = 0; j < size+1; j++) {
+			if (j == 0) {
+				cout << "  " << i + 1 << "   |";
+			}
+			else {
+				char c = ' ';
+				for (int l = 0; l < 25; l++) {
+
+					if (p[l].x == i + 1 && p[l].y == j) {
+
+						if (p[l].player == 0) {
+							c = 'X';
+						}
+						else {
+							c = 'O';
+						}
+					}
+				}
+				cout << "  " << c << "   |";
+			}
+		}
+		cout << "\n";
+		for (int z = 0; z < size; z++) {
+			cout << "----------";
+		}
 	}
 	cout << "\n";
 }
